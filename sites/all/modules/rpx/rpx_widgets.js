20046444dacfd22c0c6812468237f5c482ac5026
Antonio Estevez <tootoast@gmail.com>
2013-09-02 16:55:22 -0400

    core and contrb module update

--- sites/all/modules/rpx/rpx_widgets.js
+++ sites/all/modules/rpx/rpx_widgets.js
@@ -5,43 +5,43 @@ Drupal.behaviors.rpx = {
     function finishCallback(results) {
       var setting = settings.rpx.clicked_link_settings;
       if ('cookie' in setting) {
-        $.each(results, function(key, provider) {
-          if (provider.success) {
-            $.cookie('Drupal.visitor.rpx_social_' + setting.cookie.type, setting.cookie.id, {path: '/'});
-            location.replace(Drupal.settings.basePath + '?q=rpx/cookie_handler&destination=' + location.href);
-            return false;
-          }
-        });
+        $.cookie('Drupal.visitor.rpx_social_' + setting.cookie.type, setting.cookie.id, {path: '/'});
+        location.replace(Drupal.settings.basePath + '?q=rpx/cookie_handler&destination=' + location.href);
+        return false;
       }
-    }
+    }  	
+	try {
+		  setTimeout(function(){janrain.events.onShareSendComplete.addHandler(function(){finishCallback();});}, 300);  
+		}
+	catch (e) {
+		  setTimeout(function(){janrain.events.onShareSendComplete.addHandler(function(){finishCallback();});}, 300);  
+		}
     function popupSocial() {
-      RPXNOW.loadAndRun(['Social'], function () {
-        var post = settings.rpx.clicked_link_settings.post;
-        var activity = new RPXNOW.Social.Activity(
-          post.label,
-          post.linktext,
-          post.link
-        );
+        var post          = settings.rpx.clicked_link_settings.post;
+        var share_title   = '';
+        var share_summary = '';
+        var share_comment = '';
+        
         if ('comment' in post) {
-          activity.setUserGeneratedContent(post.comment);
+          share_comment = post.comment;
         }
         if ('summary' in post) {
-          activity.setDescription(post.summary);
+          share_summary = post.summary;
         }
         if ('title' in post) {
-          activity.setTitle(post.title);
+          share_title = post.title;
         }
-        RPXNOW.Social.publishActivity(activity, {finishCallback: finishCallback});
-      });
+        setShare(post.link, share_comment, share_title, share_summary, 'facebook');
     };
+    
     if ('rpx' in settings && 'atonce' in settings.rpx) {
       settings.rpx.clicked_link_settings = settings.rpx.atonce;
-      popupSocial();
+      setTimeout(function(){popupSocial();}, 300);     
     }
+    
     $('.rpx-link-social', context).once('rpx-link-social', function() {
       $(this).bind('click', function(e) {
         settings.rpx.clicked_link_settings = settings.rpx[$(this).attr('id')];
-        popupSocial();
         return false;
       });
     });
